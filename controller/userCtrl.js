f728e333cea856c07dedd80977f8274a963d01cc
alissonrhuant3 <alissonrhuant3@gmail.com>
2024-08-08 17:58:22 -0300

    Alterações no modelo de carrinho e na adição de produtos ao carrinho

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -323,35 +323,16 @@ const getWishlist = asyncHandler(async (req, res) => {
 });
 
 const userCart = asyncHandler(async (req, res) => {
-  const { cart } = req.body;
+  const { productId, color, quantity, price } = req.body;
   const { _id } = req.user;
   validateMongoDbId(_id);
   try {
-    let products = [];
-    const user = await User.findById(_id);
-    // Checando se o usuário já tem produtos no carrinho
-    const alreadyExistCart = await Cart.findOne({ orderBy: user._id });
-    if (alreadyExistCart) {
-      alreadyExistCart.remove();
-    }
-    for (let i = 0; i < cart.length; i++) {
-      let object = {};
-      object.product = cart[i]._id;
-      object.count = cart[i].count;
-      object.color = cart[i].color;
-      let getPrice = await Product.findById(cart[i]._id).select("price").exec();
-      object.price = getPrice.price;
-      products.push(object);
-    }
-    let cartTotal = 0;
-    for (let i = 0; i < products.length; i++) {
-      cartTotal += products[i].price * products[i].count;
-    }
-    console.log(products, cartTotal);
     let newCart = await new Cart({
-      products,
-      cartTotal,
-      orderBy: user?._id,
+      userId:_id,
+      productId,
+      color,
+      price,
+      quantity
     }).save();
     res.json(newCart);
   } catch (error) {
